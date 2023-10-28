f5b2787ae493efc4033d5a722c0c826ea841576d
Sanjay <shettysanjay719@gmail.com>
2023-10-28 16:10:15 +0530

    first commit

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer.purple({super.key})
+      : color1 = const Color.fromARGB(255, 216, 151, 131),
+        color2 = const Color.fromARGB(255, 197, 81, 46);
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
+
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+//   final List<Color> colors;
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World'),
+//       ),
+//     );
+//   }
+// }
