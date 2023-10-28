7a126445c6ada3c85f1fcec82525e6bb9b0f2e32
Sanjay <shettysanjay719@gmail.com>
2023-10-28 16:12:53 +0530

    second commit

---
+++ favorite_places/lib/providers/user_places.dart
@@ -0,0 +1,19 @@
+//Managing the places selected by users
+import 'dart:io';
+
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
